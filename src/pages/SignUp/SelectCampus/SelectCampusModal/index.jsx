82c9b2e420dd0322bc7b4c2a2fb2eca65cb900d7
-v <elielbarbosads@gmail.com>
2026-04-28 23:31:14 -0300

    feat(styles): Alterar estilos do modal da página de seleção de campus.

--- src/pages/SignUp/SelectCampus/SelectCampusModal/index.jsx
+++ src/pages/SignUp/SelectCampus/SelectCampusModal/index.jsx
@@ -1,32 +1,40 @@
 import SelectCampusCard from "../SelectCampusCard";
 import styles from "./styles.module.css";
-import iconClose from "../../../../assets/icons/close.svg";
+
+import checkIcon from "../../../../assets/icons/check.svg";
 import { useContext } from "react";
 import { UserContext } from "../../../../contexts/UserContext";
 
 function SelectCampusForm() {
-  const { mostrar, setMostrar, createNewUser } = useContext(UserContext);
+  const { setMostrar, createNewUser } = useContext(UserContext);
 
   return (
     <div className={`${styles.campusModalContainer} opacityAnimation`}>
       <div className={`${styles.modalCampus} scaleAnimation`}>
-        <div className={styles.wrapperButtonClose}>
-          <button onClick={() => setMostrar(!mostrar)}>
-            <img src={iconClose} alt="" srcset="" />
-          </button>
-        </div>
+        <img src={checkIcon} alt="Check Icon" className={styles.checkIcon} />
+        <h5>Confirmação de Seleção</h5>
         <p className={styles.modalCampusMessage}>
           Você selecionou o campus IFS - Tobias Barreto, deseja confirmar?
         </p>
-        <button
-          onClick={() => {
-            setMostrar(false);
-            createNewUser();
-          }}
-          className={styles.modalConfirmButton}
-        >
-          Confirmar
-        </button>
+        <div className={`${styles.modalButtonContainer} mt-2`}>
+          <button
+            onClick={() => {
+              setMostrar(false);
+            }}
+            className={`${styles.modalCancelButton} secondary`}
+          >
+            Cancelar
+          </button>
+          <button
+            onClick={() => {
+              setMostrar(false);
+              createNewUser();
+            }}
+            className={`${styles.modalConfirmButton} primary`}
+          >
+            Confirmar
+          </button>
+        </div>
       </div>
     </div>
   );
